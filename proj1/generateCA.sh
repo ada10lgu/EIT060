0377bb31cfb96de6581f58e3f5c2dc5fc89f68d3
Lars Gustafson <lars.gustafson@gmail.com>
2016-01-29 12:36:21 +0100

    _

--- proj1/generateCA.sh
+++ proj1/generateCA.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+KEY="key.pem";
+CERT="cert.pem";
+
 #Generate Certificate
 function generateCA509 {
-	echo "hej"
-	openssl req -x509 -newkey rsa:1024 -keyout key.pem -out cert.pem	
+	openssl req -x509 -newkey rsa:1024 -keyout $KEY -out $CERT	
 
 }
 
 # Generate trustStore and import CA-certificate
 function trustStore {
-	keytool -import -v -trustcacerts -file cert.pem -keystore clienttruststore -keypass password
+	keytool -import -v -trustcacerts -file $CERT -keystore clienttruststore -keypass password
+}
+
+function eit060_all {
+	generateCA509;
+	trustStore;
 }
