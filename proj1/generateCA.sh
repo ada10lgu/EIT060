267b794999954e78f8577cd4c9ffa6712a4ac74b
David Norrestam <fys11dno@lo-9.student.lth.se>
2016-02-02 12:39:30 +0100

    2.1.1-2.1.7 done

--- proj1/generateCA.sh
+++ proj1/generateCA.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-KEY="key.pem";
-CERT="cert.pem";
+KEY="key.crt";
+CERT="ca.crt";
 CONFIG="config";
 NAMES="cn=\"DavidNorrestam(fys11dno)/HannesFornell(cek11hfo)/LarsGustafsson(ada10lgu)/HannaAndreason(bte12han)\"";
 KSNAME="clientkeystore";
 TSNAME="clienttruststore";
+CSRNAME="client.csr";
+ALIAS="EIT060";
+CAROOT="theCARoot";
+
 
 #Generate Certificate
 function generateCA509 {
@@ -16,16 +20,38 @@ function generateCA509 {
 # Generate trustStore and import CA-certificate
 function trustStore {
 	rm $TSNAME
-	keytool -import -v -trustcacerts -file $CERT -keystore $TSNAME -keypass password
+	keytool -import -trustcacerts -v -file $CERT -keystore $TSNAME -keypass password -alias $CERT
 }
 
 function eit060_all {
 	generateCA509;
 	trustStore;
 	keyStore;
+	generateCSR;
+	sign;
+	import;
+	printStuff;
 }
+
 
 function keyStore {
 	rm $KSNAME
-	keytool -genkeypair -keystore $KSNAME -storepass password -dname $NAMES
+	keytool -genkeypair -keystore $KSNAME -storepass password -dname $NAMES -alias client
+}
+
+function generateCSR {
+	keytool -certreq -keystore $KSNAME -keyalg rsa:1024 -alias client -file $CSRNAME 
+}
+
+function sign {
+	openssl x509 -req -in $CSRNAME -CA $CERT -CAkey $KEY -CAcreateserial -out client.cer
+}
+
+function import {
+	keytool -import -trustcacerts -keystore $KSNAME -file $CERT -alias $CAROOT
+	keytool -import -file client.cer -keystore $KSNAME -alias client
+}
+
+function printStuff {
+	keytool -list -v -keystore $KSNAME
 }
