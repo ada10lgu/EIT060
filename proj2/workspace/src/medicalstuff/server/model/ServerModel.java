74cc857cbc951abe137471970b8b56180e5b58e7
Lars Gustafson <lars.gustafson@gmail.com>
2016-02-17 01:01:29 +0100

    Server now loads logfile but no logging is done

--- proj2/workspace/src/medicalstuff/server/model/ServerModel.java
+++ proj2/workspace/src/medicalstuff/server/model/ServerModel.java
@@ -13,6 +13,7 @@ import javax.net.ssl.SSLSocket;
 
 import medicalstuff.general.connection.ConnectionHandler;
 import medicalstuff.general.connection.SecureServer;
+import medicalstuff.server.model.data.log.Logger;
 import medicalstuff.server.model.data.user.User;
 import medicalstuff.server.model.data.user.UserList;
 
@@ -20,14 +21,19 @@ public class ServerModel implements ConnectionHandler {
 
 	private SecureServer ss;
 	private UserList users;
+	private Logger logger;
 
 	public ServerModel(int port, File keystore, File truststore, char[] password) throws KeyManagementException,
 			UnrecoverableKeyException, KeyStoreException, NoSuchAlgorithmException, CertificateException, IOException {
+
+		System.out.print("Loading logfile...");
+		logger = new Logger();
+		System.out.println("done");
+		
 		System.out.print("Loading users...");
 		users = new UserList();
 		System.out.println("loaded " +users.size() + " users");
 
-		//users.addUser("Hanna Andreason", ""+123, 0);
 		
 		ss = new SecureServer(port, this, keystore, truststore, password);
 		ss.start();
