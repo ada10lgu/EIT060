81f39d092568e44675136dc7b6912a2eea2e8f2f
Lars Gustafson <lars.gustafson@gmail.com>
2016-02-17 12:34:07 +0100

    Menu

--- proj2/workspace/src/medicalstuff/client/gui/medicalstuff/menu/Menu.java
+++ proj2/workspace/src/medicalstuff/client/gui/medicalstuff/menu/Menu.java
@@ -1,5 +1,7 @@
 package medicalstuff.client.gui.medicalstuff.menu;
 
+import java.awt.event.KeyEvent;
+
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
@@ -9,16 +11,16 @@ import medicalstuff.client.model.ClientModel;
 public class Menu extends JMenuBar {
 	public Menu(ClientModel model) {
 		JMenu main = new JMenu("File");
+		main.setMnemonic(KeyEvent.VK_F);
 		main.add(new LogoutItem(model));
 		main.addSeparator();
 		main.add(new ExitItem(model));
 		JMenu help = new JMenu("Help");
+		help.setMnemonic(KeyEvent.VK_H);
 		help.add(new ServerInfo(model));
 		help.add(new About());
 		
-		
 		add(main);
 		add(help);
-
 	}
 }
