5068e8228b172f2ece90a8cad0b9e990705f6c90
David <david.norrestam@hotmail.com>
2016-02-24 16:27:19 +0100

    Updated GetJournalPacket

--- proj2/workspace/src/medicalstuff/general/medicalpackets/packets/GetJournalPacket.java
+++ proj2/workspace/src/medicalstuff/general/medicalpackets/packets/GetJournalPacket.java
@@ -2,31 +2,33 @@ package medicalstuff.general.medicalpackets.packets;
 
 import medicalstuff.general.connection.packets.Packet;
 import medicalstuff.general.connection.packets.operands.OperatorPacket;
+import medicalstuff.general.medicalpackets.MedicalModel;
+import static medicalstuff.general.medicalpackets.MedicalFactory.GET_JOURNAL_PACKET;
 
 public class GetJournalPacket extends OperatorPacket {
-
+	private MedicalModel model;
+	public GetJournalPacket(byte[] data, Packet[] packets, MedicalModel model) {
+		this.model = model;
+	}
+	
 	@Override
 	public OperatorPacket perform() {
-		// TODO Auto-generated method stub
-		return null;
+		
 	}
 
 	@Override
 	protected byte[] getLoad() {
-		// TODO Auto-generated method stub
-		return null;
+		return new byte[0];
 	}
 
 	@Override
 	protected Packet[] getPackages() {
-		// TODO Auto-generated method stub
-		return null;
+		return new Packet[0];
 	}
 
 	@Override
 	protected byte getType() {
-		// TODO Auto-generated method stub
-		return 0;
+		return GET_JOURNAL_PACKET;
 	}
 	
 }
