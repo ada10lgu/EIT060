c2e077fadaa769a2eaa037cf98c30b7a71b0dbfe
ada10lgu <ada10lgu@falk-3.student.lth.se>
2016-03-03 21:25:43 +0100

    Allowed for paramaters to start server and lcient

--- proj2/workspace/src/medicalstuff/client/MedicalClient.java
+++ proj2/workspace/src/medicalstuff/client/MedicalClient.java
@@ -1,13 +1,45 @@
 package medicalstuff.client;
 
+import java.util.HashMap;
+
 import medicalstuff.client.gui.ClientGUI;
 import medicalstuff.client.model.ClientModel;
 
 public class MedicalClient {
 	public static void main(String[] args) {
-		String certFolder = "certs/";
+		
+		HashMap<String,String> settings = new HashMap<>();
+		settings.put("certFolder","certs/");
+		settings.put("ip", "localhost");
+		settings.put("port", "12345");
+		
+		for (int i = 0; i < args.length; i++) {
+			String s = args[i];
+			if (!s.startsWith("-")) {
+				System.out.println("Unknown command \""+s+"\"");
+				System.exit(0);
+			}
+			String command = s.substring(1);
+			String old = settings.put(command, args[++i]);
+			if (old == null) {
+				System.out.println("Unknown command \""+s+"\"");
+				System.exit(0);
+			}
+		}
+		
+		System.out.println("The follow settings are beeing used:");
+		System.out.println(settings);
+		
+		int port = -1;
+		try {
+			port = Integer.parseInt(settings.get("port"));
+		} catch (NumberFormatException e) {
+			System.out.println("Can't parse port");
+			System.exit(0);
+		}
+		
 
-		ClientModel model = new ClientModel("localhost", 12345, certFolder);
+		ClientModel model = new ClientModel(settings.get("ip"), port, settings.get("certFolder"));
 
 		new ClientGUI(model);
 	}
--- proj2/workspace/src/medicalstuff/server/MedicalServer.java
+++ proj2/workspace/src/medicalstuff/server/MedicalServer.java
@@ -7,19 +7,56 @@ import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
 import java.security.cert.CertificateException;
+import java.util.HashMap;
 
 import medicalstuff.server.model.ServerModel;
 
 public class MedicalServer {
-	public static void main(String[] args) throws KeyManagementException,
-			UnrecoverableKeyException, KeyStoreException,
-			NoSuchAlgorithmException, CertificateException, IOException {
+	public static void main(String[] args) {
 
-		char[] password = "password".toCharArray();
-		int port = 12345;
-		File keystore = new File("certs/server_key");
-		File truststore = new File("certs/server_trust");
+		HashMap<String, String> settings = new HashMap<>();
+		settings.put("certFolder", "certs/");
+		settings.put("password", "password");
+		settings.put("port", "12345");
 
-		new ServerModel(port, keystore, truststore, password, false);
+		for (int i = 0; i < args.length; i++) {
+			String s = args[i];
+			if (!s.startsWith("-")) {
+				System.out.println("Unknown command \"" + s + "\"");
+				System.exit(0);
+			}
+			String command = s.substring(1);
+			String old = settings.put(command, args[++i]);
+			if (old == null) {
+				System.out.println("Unknown command \"" + s + "\"");
+				System.exit(0);
+			}
+		}
+
+		System.out.println("The follow settings are beeing used:");
+		System.out.println(settings);
+		System.out.println();
+		char[] password = settings.get("password").toCharArray();
+
+		int port = -1;
+		try {
+			port = Integer.parseInt(settings.get("port"));
+		} catch (NumberFormatException e) {
+			System.out.println("Can't parse port");
+			System.exit(0);
+		}
+
+		String certFolder = settings.get("certFolder");
+
+		File keystore = new File(certFolder + "server_key");
+		File truststore = new File(certFolder + "server_trust");
+
+		try {
+			new ServerModel(port, keystore, truststore, password, false);
+		} catch (KeyManagementException | UnrecoverableKeyException | KeyStoreException | NoSuchAlgorithmException
+				| CertificateException | IOException e) {
+			System.out.println("Could not start server!");
+			System.exit(0);
+		}
 	}
 }
