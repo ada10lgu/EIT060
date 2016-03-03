285f442097cc0bb720c119c44bfafbf40bea6f00
ada10lgu <ada10lgu@falk-3.student.lth.se>
2016-03-03 19:35:07 +0100

    added gui pane

---
+++ proj2/workspace/src/medicalstuff/client/gui/medicalstuff/journal/EntryListPane.java
@@ -0,0 +1,13 @@
+package medicalstuff.client.gui.medicalstuff.journal;
+
+import javax.swing.JPanel;
+
+import medicalstuff.client.model.Journal;
+
+@SuppressWarnings("serial")
+public class EntryListPane extends JPanel {
+
+	public void setJournal(Journal j) {
+		System.out.println("hej");
+	}
+}
